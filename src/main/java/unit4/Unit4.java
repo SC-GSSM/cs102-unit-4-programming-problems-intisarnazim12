acc9cc09b3ee51f6d6b70bcf52d2f10e5d48a90a
intisarnazim12 <intisarnazim12@gmail.com>
2026-03-30 13:47:21 +0000

    did question 4

--- src/main/java/unit4/Unit4.java
+++ src/main/java/unit4/Unit4.java
@@ -115,8 +115,15 @@ public class Unit4 {
      * @return the number of vowels found
      */
     public int countVowels(String str) {
+       int count = 0;
+       for (int i = 0; i < str.length(); i++) {
+        char ch = Character.toLowerCase(str.CharAt(i));
+        if (ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u") {
+            count++;
+        }
+       }
         // TODO: Implement this method
-        return 0;
+        return count;
     }
 
     /**
