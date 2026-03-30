3cba174a1be76f88bfac260901aba2d50299fc52
intisarnazim12 <intisarnazim12@gmail.com>
2026-03-30 13:56:55 +0000

    finished

--- src/main/java/unit4/Unit4.java
+++ src/main/java/unit4/Unit4.java
@@ -117,8 +117,8 @@ public class Unit4 {
     public int countVowels(String str) {
        int count = 0;
        for (int i = 0; i < str.length(); i++) {
-        char ch = Character.toLowerCase(str.CharAt(i));
-        if (ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u") {
+        char ch = Character.toLowerCase(str.charAt(i));
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
             count++;
         }
        }
